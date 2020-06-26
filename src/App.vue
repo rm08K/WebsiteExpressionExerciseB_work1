4d4c2d1fd845290480499b4bc07c1b49e6465230
rm08K <a18dc175@dhw.ac.jp>
2020-06-26 21:41:33 +0900

    day1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <myheader></myheader>
+        <p v-if="msg.length > 0">
+        {{msg}}
+        </p>
+        <p v-else>no text</p>
+        <input type="text" v-model="msg">
+        <button @click="clear()">clear</button>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+import myheader from './components/myheader'
+    export default {
+        components: {
+            myheader
+        },
+        data () {
+            return {
+            msg: 'Hello World!'
+            }
+        },
+        methods: {
+            clear () {
+                this.msg = ''
+                }
+        }
+    }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
